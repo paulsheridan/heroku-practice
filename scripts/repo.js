e72580acb67c6a89e523dfff4ae02a8589ec329d
Paul Sheridan <paul.sheridan@me.com>
2016-01-20 12:10:08 -0800

    update

--- scripts/repo.js
+++ scripts/repo.js
@@ -17,6 +17,16 @@
   //   }).done(callback);
   // };
 
+  $.ajax({
+    url: '/github/users/paulsheridan/repos' +
+          '?per_page=100' + '&sort=updated',
+    type: 'GET',
+    success: function(data, message, xhr) {
+      repos.all = data;
+    }
+  }).done(callback);
+};
+
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
       return repo[attr];
