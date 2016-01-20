fbde0f1926c2901bb413abcfdf5567bbdbc92237
Paul Sheridan <paul.sheridan@me.com>
2016-01-20 14:08:56 -0800

    removed github Token

--- scripts/repo.js
+++ scripts/repo.js
@@ -3,33 +3,12 @@
 
   repos.all = [];
 
-  // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
+  // DONE: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   // repos.requestRepos = function(callback) {
-  //   $.ajax({
-  //     url: 'https://api.github.com/users/paulsheridan/repos' +
-  //           '?per_page=100' +
-  //           '&sort=updated',
-  //     type: 'GET',
-  //     headers: { 'Authorization': 'token ' + githubToken },
-  //     success: function(data, message, xhr) {
-  //       repos.all = data;
-  //     }
-  //   }).done(callback);
-  // };
-// repos.requestRepos = function(callback) {
-//   $.ajax({
-//     url: '/github/users/paulsheridan/repos' +
-//           '?per_page=100' + '&sort=updated',
-//     type: 'GET',
-//     success: function(data, message, xhr) {
-//       repos.all = data;
-//     }
-//   }).done(callback);
-// };
 
 repos.requestRepos = function(callback) {
   $.get('/github/users/paulsheridan/repos', function(data) {
-    console.log(data);
+    // console.log(data);
     repos.all = data;
   }).done(callback);
 };
